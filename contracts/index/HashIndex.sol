56de309ab4bbef9748610c372731151df0d59995
bylee <bylee78@gmail.com>
2021-01-02 04:04:41 +0900

    Add values

--- contracts/index/HashIndex.sol
+++ contracts/index/HashIndex.sol
@@ -7,26 +7,44 @@ import './Index.sol';
 
 contract HashIndex is Index {
   string private constant ERR_EMPTY_KEY = 'EMPTY_KEY';
-  mapping (string => string) value2key;
-  mapping (string => uint) key2count;
+  string[] keys;
+  mapping (string => uint) key2sequence;
+  mapping (string => string[]) key2values;
+  mapping (string => uint) value2sequence;
 
   function add(string memory key, string memory value) public override {
     require(StringUtils.isNotEmpty(key), ERR_EMPTY_KEY);
-    string memory oldKey = value2key[value];
-    if (StringUtils.isNotEmpty(oldKey)) {
+    uint valueSeq = value2sequence[value];
+    if (0 == valueSeq) {
+      // insert
+      key2values[key].push(value);
+      value2sequence[value] = key2values[key].length;
+      keys.push(key);
+      key2sequence[key] = keys.length;
+    } else {
       // update
-      key2count[oldKey]--;
+      uint oldIndex = value2sequence[value];
+      key2values[key][oldIndex] = value;
     }
-    value2key[value] = key;
-    key2count[key]++;
   }
 
   function remove(string memory key, string memory value) public override {
     require(StringUtils.isNotEmpty(key), ERR_EMPTY_KEY);
-    string memory oldKey = value2key[value];
-    if (StringUtils.isNotEmpty(oldKey)) {
-      delete value2key[value];
-      key2count[oldKey]--;
+    uint valueSequence = value2sequence[value];
+    if (0 != valueSequence) {
+      uint movingValueSequence = key2values[key].length;
+      string memory movingValue = key2values[key][movingValueSequence - 1];
+      key2values[key][valueSequence - 1] = movingValue;
+      delete key2values[key][movingValueSequence - 1];
+      value2sequence[movingValue] = valueSequence;
+      if (movingValueSequence == 1) {
+        uint keySequence = key2sequence[key];
+        uint movingKeySequence = keys.length;
+        string memory movingKey = keys[movingKeySequence - 1];
+        keys[keySequence - 1] = movingKey;
+        key2sequence[movingKey] = keySequence;
+        delete keys[movingKeySequence - 1];
+      }
     }
   }
 
@@ -40,6 +58,6 @@ contract HashIndex is Index {
   function countBy(ValuePoint memory start, ValuePoint memory end) public view override returns (uint) {
     require(start.boundType == 0 && end.boundType == 0, 'UNSUPPORTED_ARG');
     require(0 == StringUtils.compare(start.value, end.value), 'UNSUPPORTED_ARG');
-    return key2count[start.value];
+    return key2values[start.value].length;
   }
 }
